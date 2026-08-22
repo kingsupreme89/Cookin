96d7983835707fb21bd8cc975e5d988baf80e1f7
KingSupreme <user@KingBook-Air.local>
2026-08-22 12:12:28 -0600

    Initialize Expo mobile app

---
+++ apps/mobile/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Cookin' with B</Text>
+      <Text style={styles.subtitle}>Your personalized digital pantry</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subtitle: {
+    marginTop: 10,
+    color: '#666',
+  }
+});
